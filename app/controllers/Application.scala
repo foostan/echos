d2eb2fffe590ebd93ae9b2f427234950ac962952
foostan <foostan27@gmail.com>
2014-07-25 02:44:14 +0900

    Add pages for task

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,27 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Task
+
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.tasks)
   }
+
+  def tasks = Action {
+    val taskForm = Form(
+      "label" -> nonEmptyText
+    )
+
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
