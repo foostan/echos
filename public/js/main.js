dba719ad1d19b7cb890da9284b5173771054215a
foostan <foostan27@gmail.com>
2014-07-30 02:27:33 +0900

    appear result after submited

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function () {
-    function echo () {
+    function echo() {
         var message = $("#post-message").val();
         $.ajax({
             type: "POST",
@@ -9,16 +9,31 @@
                 number: 10
             },
             success: function (data) {
-                $("#echo-result table").html("").append("<tr><td>" + data.echo + "</td></tr>");
-                $("#echos-result table").html("");
-                jQuery.each(data.echos, function() {
-                    $("#echos-result table").append("<tr><td>" + this + "</td></tr>");
+
+
+                $("#result").children().remove();
+                var $echo_result = $('<div class="panel panel-default"></div>')
+                    .append('<div class="panel-heading">echo</div>')
+                    .append('<table class="table"><tr><td>' + data.echo + '</td></tr></table>');
+
+                var $echos_result_table = $('<table class="table"></table>');
+                jQuery.each(data.echos, function () {
+                    $echos_result_table.append("<tr><td>" + this + "</td></tr>");
                 });
+
+                var $echos_result = $('<div class="panel panel-default"></div>')
+                    .append('<div class="panel-heading">echos</div>')
+                    .append($echos_result_table);
+
+                $("#result").append($echo_result);
+                $("#result").append($echos_result);
+
+
             }
         });
     }
 
-    $("#post-message").keydown(function(e) {
+    $("#post-message").keydown(function (e) {
         if (e.keyCode == 13) {
             echo();
             return false;
